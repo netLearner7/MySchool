2a79a1a0b125200a3af8b11457dbff477ef5a4f6
netLearner7 <1044196262@qq.com>
2019-03-05 13:00:21 +0800

    完善学生表的信息

--- MySchool/Core/Models/Student.cs
+++ MySchool/Core/Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,13 +12,20 @@ namespace MySchool.Core.Models
     {
         public int Id { get; set; }
 
+        [StringLength(8, ErrorMessage = "不可以超过8个字符")]
         [DisplayName("学生名称")]
         public string RealName { get; set; }
 
         [DisplayName("入学时间")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
         public DateTime EnrollmentDate { get; set; }
+
 
         [DisplayName("入学信息")]
         public ICollection<Enrollment> Enrollments { get; set; }
+
+        [MaxLength(200)]
+        public string secret { get; set; }
     }
 }
